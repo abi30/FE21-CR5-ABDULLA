c4db57216b3b04e1cd81804aa082fc81e25e23e4
Abdulla Al Rakib <36118180+abi30@users.noreply.github.com>
2021-04-05 21:44:44 +0200

    fixed somebug

--- output/script.js
+++ output/script.js
@@ -1,4 +1,3 @@
-"use strict";
 // console.log("hello");
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
@@ -78,18 +77,18 @@ var Events = /** @class */ (function (_super) {
     return Events;
 }(Locations));
 // ----------------------3 objects from Locations---------------------
-new Locations("place", "turkish mosque ", "turkey", "The Hagia Sophia has just opened to receive Sunni Muslim worshippers for the first time since 1931.", 34122, "Sultan Ahmet, Atmeydan", "../img/places1.jpg", new Date("Fri Jan 18 2015 13:30:50"));
+new Locations("place", "turkish mosque ", "turkey", "The Hagia Sophia has just opened to receive Sunni Muslim worshippers for the first time since 1931.", 34122, "Sultan Ahmet, Atmeydan", "places1.jpg", new Date("Fri Jan 18 2015 13:30:50"));
 // console.log(myTravels)
-new Locations("place", "Eiffel Tower", "Paris", "The Eiffel Tower is 1,063 feet (324 meters) tall, including the antenna at the top. Without the antenna, it is 984 feet 300 m.", 75007, "Champ de Mars, 5 Avenue Anatole France", "../img/places3.jpg", new Date("Fri Jan 08 2013 12:44:57"));
-new Locations("place", "time Square", "new york", "Times Square is a major commercial intersection, tourist destination, entertainment center, and neighborhood in the Midtown Manhattan section of New York City", 10036, "Manhattan, NY ", "../img/places4.jpg", new Date("Fri Feb 28 2012 07:44:57"));
+new Locations("place", "Eiffel Tower", "Paris", "The Eiffel Tower is 1,063 feet (324 meters) tall, including the antenna at the top. Without the antenna, it is 984 feet 300 m.", 75007, "Champ de Mars, 5 Avenue Anatole France", "places3.jpg", new Date("Fri Jan 08 2013 12:44:57"));
+new Locations("place", "time Square", "new york", "Times Square is a major commercial intersection, tourist destination, entertainment center, and neighborhood in the Midtown Manhattan section of New York City", 10036, "Manhattan, NY ", "places4.jpg", new Date("Fri Feb 28 2012 07:44:57"));
 // ----------------------3 objects from Restaurants---------------------
-new Restaurants("Restaurant", "Medusa Restaurant & Club", "Viennese", "Vienna", "there’s anybody who’s fit to navigate Vienna’s approximately 4,000 restaurants, it’s Lukas Hittinger", 1010, "Neuer Markt 8", "../img/restaurants1.jpg", new Date("Fri Jan 05 2021 09:44:57"), "http://www.medusarestaurant.at", "+43 676 3094176");
-new Restaurants("Restaurant", "Eater At Home", "Chinese", "Chaina", "When traveling through China, you may have trouble finding your most beloved beverages from back home,", 43280, "Haizingergasse 13,", "../img/restaurants4.jpg", new Date("Fri Dec 11 2014 21:44:57"), "https://www.eater.com/contact", "+43654321");
-new Restaurants("Restaurant", "Tawa Indian Restaurant", "Indian", "vienna", "If you love Indian food, you’re probably familiar with chicken tikka masala, which appears on most Indian restaurant menus", 1120, "Am Hauptbahnhof 1", "../img/restaurants5.jpg", new Date("Sat Oct 20 2017 07:44:57"), "https://www.viennawurstelstand.com/guide/best-indian-restaurants-in-vienna/", "+436546721");
+new Restaurants("Restaurant", "Medusa Restaurant & Club", "Viennese", "Vienna", "there’s anybody who’s fit to navigate Vienna’s approximately 4,000 restaurants, it’s Lukas Hittinger", 1010, "Neuer Markt 8", "restaurants1.jpg", new Date("Fri Jan 05 2021 09:44:57"), "http://www.medusarestaurant.at", "+43 676 3094176");
+new Restaurants("Restaurant", "Eater At Home", "Chinese", "Chaina", "When traveling through China, you may have trouble finding your most beloved beverages from back home,", 43280, "Haizingergasse 13,", "restaurants4.jpg", new Date("Fri Dec 11 2014 21:44:57"), "https://www.eater.com/contact", "+43654321");
+new Restaurants("Restaurant", "Tawa Indian Restaurant", "Indian", "vienna", "If you love Indian food, you’re probably familiar with chicken tikka masala, which appears on most Indian restaurant menus", 1120, "Am Hauptbahnhof 1", "restaurants5.jpg", new Date("Sat Oct 20 2017 07:44:57"), "https://www.viennawurstelstand.com/guide/best-indian-restaurants-in-vienna/", "+436546721");
 // ----------------------3 objects from events---------------------
-new Events("Events", "concert stadthalle", "Vienna", "In 1956, Vienna acquired the Vienna Stadthalle, thanks to architect Roland Rainer, as a flagship for modern concerts and shows", 1150, "Roland-Rainer-Platz 1", "../img/events1.jpg", "www.google.com", "+4343123", new Date("Fri Dec 08 2020 07:44:57"), 45);
-new Events("Events", "stadium dhaka", "dhaka", "It was hard to get the tickets, but a kindly young chap from Singapore had helped this stranger buy the tickets.", 1216, "shawrapara 456", "../img/events4.jpg", "www.google.com", "+880343123", new Date("Fri Dec 08 2019 07:44:57"), 15);
-new Events("Events", "convention hall", "New york", "A convention center is a large building that is designed to hold a convention, where individuals and groups gather to promote and share common interests.", 4324, "michail man road 34/3", "../img/events2.jpg", "www.google.com", "+4343123", new Date("Sun Dec 08 2018 18:30:00"), 50);
+new Events("Events", "concert stadthalle", "Vienna", "In 1956, Vienna acquired the Vienna Stadthalle, thanks to architect Roland Rainer, as a flagship for modern concerts and shows", 1150, "Roland-Rainer-Platz 1", "events1.jpg", "www.google.com", "+4343123", new Date("Fri Dec 08 2020 07:44:57"), 45);
+new Events("Events", "stadium dhaka", "dhaka", "It was hard to get the tickets, but a kindly young chap from Singapore had helped this stranger buy the tickets.", 1216, "shawrapara 456", "events4.jpg", "www.google.com", "+880343123", new Date("Fri Dec 08 2019 07:44:57"), 15);
+new Events("Events", "convention hall", "New york", "A convention center is a large building that is designed to hold a convention, where individuals and groups gather to promote and share common interests.", 4324, "michail man road 34/3", "events2.jpg", "www.google.com", "+4343123", new Date("Sun Dec 08 2018 18:30:00"), 50);
 // console.log(myTravels);
 var content = "";
 myTravels.forEach(function (travel) { return content += createCard(travel); });
@@ -98,7 +97,7 @@ document.querySelector("[data-meta=\"entry\"]").innerHTML = content;
 // ###################### myAllFunc #########################
 myAllFunc();
 function createCard(travel) {
-    var allVal = "<div class=\"col\">\n\n        <div class=\"card h-100 own_card \" style=\"background-color:\">\n            <img class=\"img-fluid own\" src=\"" + travel.img + "\" class=\"card-img-top\" alt=\"...\">\n\n\n            <div class=\"card-body text-center\">\n            <h2 class=\"card-title\">" + travel.place_type + "</h2>";
+    var allVal = "<div class=\"col\">\n\n        <div class=\"card h-100 own_card \" style=\"background-color:\">\n            <img class=\"img-fluid own\" src=\"img/" + travel.img + "\" class=\"card-img-top\" alt=\"...\">\n\n\n            <div class=\"card-body text-center\">\n            <h2 class=\"card-title\">" + travel.place_type + "</h2>";
     if (travel instanceof Restaurants) {
         allVal += "<h5 class=\"list-group-item\"><b>origin:</b>" + travel.res_type + "</h5>";
     }
@@ -178,9 +177,9 @@ function sorting_desc() {
 }
 // ###################### extra search function #########################
 function search() {
-    var _a;
     //  newMyTravels="";
-    var searchText = (_a = document.getElementById('search_text')) === null || _a === void 0 ? void 0 : _a.value;
+    var searchText = document.getElementById('search_text').value;
+    console.log("hello");
     newMyTravels = [];
     for (var i = 0; i < myTravels.length; i++) {
         var entireText1 = myTravels[i].placeName;
@@ -206,3 +205,4 @@ function search() {
         document.getElementById("contact").innerHTML;
     }
 }
+console.log("hello");
